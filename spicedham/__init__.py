bd33ac7742d762a004a0a869ca5c1de0edfe5151
Ian Kronquist <iankronquist@gmail.com>
2014-07-30 10:52:31 -0700

    Use sqlalchemy - math changes - db changes

--- spicedham/__init__.py
+++ spicedham/__init__.py
@@ -1,56 +1,99 @@
 # -*- coding: utf-8 -*-
 import json
 
-from spicedham.models import WordProbability
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+from models import WordProbability
+from models import Base
 
 
-def train_bulk(file_name):
-    """Train model on input api data with an additional "spam" key"""
-    f = open(filename, 'r')
-    trainingdata = json.load(f)
-    for result in trainingdata['result']:
-        train(result)
+class SpicedHam(object):
 
 
-def train(result, is_spam):
-    try:
-        total = WordProbability.objects.get(word='*')
-    except DoesNotExist, e:
-        total = WordProbability(word='*', numTotal=0, numSpam=0)
-    for description in result['description'].split(' '):       
-        if description == '*':
-            continue
-        total.numTotal += 1
+    def __init__(self):
+        self.engine = create_engine('sqlite:///:memory:')
+        self.sessionFactory = sessionmaker()
+        self.sessionFactory.configure(bind=self.engine)
+        Base.metadata.create_all(self.engine)
+
+    def train_bulk(self, file_name):
+        """Train model on input api data with an additional "spam" key"""
+        f = open(self, filename, 'r')
+        trainingdata = json.load(self, f)
+        for result in trainingdata['result']:
+            train(self, result)
+
+
+    def train(self, result, is_spam):
+        session = self.sessionFactory()
+        query = session.query(WordProbability).filter(WordProbability.word=='*')
         try:
-            word = WordProbability.objects.get(word=description)
-        except DoesNotExist, e:
-            word = WordProbability(numTotal=0, numSpam=0, word=description)
-        word.numTotal += 1
-        if is_spam:
-            word.numSpam += 1
-            total.numSpam += 1
-        word.save()
-    total.save()
+            total = query.one()
+        except NoResultFound, e:
+            total = WordProbability()
+            total.word = '*'
+            total.numTotal = 0
+            total.numSpam = 0
+        for description in result['description'].split(' '):
+            description = description.lower()
+            if description == '*':
+                continue
+            total.numTotal += 1
+            query = session.query(WordProbability).filter(WordProbability.word==description)
+            try:
+                word = query.one()
+            except NoResultFound, e:
+                word = WordProbability()
+                word.numTotal = 0
+                word.numSpam = 0
+                word.word = description
+            word.numTotal += 1
+            if is_spam:
+                word.numSpam += 1
+                total.numSpam += 1
+            session.add(word)
+        session.add(total)
+        session.commit()
 
 
-def is_spam_from_json(json_response):
-    return is_spam(json.loads(json_response))
+    def is_spam_from_json(self, json_response):
+        return self.is_spam(json.loads(self, json_response))
 
 
-def is_spam(response):
-    pSpamGivenBigram = pSpam
-    pHamGivenBigram = pHam
-    # If this doesn't exist then the DB hasn't been trained
-    total = WordProbability.objects.get(word='*')
-    for bigram in response['description_bigrams']:
-        try:
-            word = WordProbability.objects.get(word=bigram)
-        except DoesNotExist, e:
-            continue
-        pBigram = word.numTotal / total.numTotal
-        pBigramGivenSpam = word.numSpam / word.numTotal
-        pBigramGivenHam = (word.numTotal - word.numSpam) / word.numTotal
-        pSpamGivenBigram *= pBigramGivenSpam / pBigram
-        pHamGivenBigram *= pBigramGivenHpam / pBigram
-    pSpam = pSpamGivenBigram / pHamGivenBigram
-    return pSpam
+    def is_spam(self, response):
+        # If this doesn't exist then the DB hasn't been trained
+        session = self.sessionFactory()
+        query = session.query(WordProbability).filter(WordProbability.word=='*')
+        total = query.one()
+        import q
+
+        pSpamGivenBigram = float(total.numSpam) / float(total.numTotal)
+        q(total.numSpam)
+        q(total.numTotal)
+        q(pSpamGivenBigram)
+        pHamGivenBigram = 1.0 - pSpamGivenBigram
+        q(pHamGivenBigram)
+        for description in response['description'].split(' '):
+            try:
+                query = session.query(WordProbability).filter(WordProbability.word==description)
+                word = query.one()
+            except NoResultFound, e:
+                continue
+            if word.numSpam == 0:
+                continue
+            q(total.numTotal)
+            q(word.numTotal)
+            q(word.numSpam)
+            pBigram = float(word.numTotal) / float(total.numTotal)
+            q(pBigram)
+            pBigramGivenSpam = float(word.numSpam) / float(word.numTotal)
+            q(pBigramGivenSpam)
+            pBigramGivenHam = float(word.numTotal - word.numSpam) / float(word.numTotal)
+            q(pBigramGivenHam)
+            pSpamGivenBigram *= float(pBigramGivenSpam) / float(pBigram)
+            pHamGivenBigram *= float(pBigramGivenHam) / float(pBigram)
+            q(pSpamGivenBigram)
+            q(pHamGivenBigram)
+        pSpam = float(pSpamGivenBigram) / float(pHamGivenBigram)
+        return pSpam
--- spicedham/models.py
+++ spicedham/models.py
@@ -1,9 +1,13 @@
-from django.db import models as djmodels
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-class WordProbability(djmodels.Model):
-    word = djmodels.CharField(max_length=255, primary_key=True)
-    numSpam = djmodels.IntegerField()
-    numTotal = djmodels.IntegerField()
+Base = declarative_base()
+
+class WordProbability(Base):
+    __tablename__ = 'word_probability'
+    word = Column(String, primary_key=True)
+    numSpam = Column(Integer)
+    numTotal = Column(Integer)
 
     def __unicode__(self):
         return self.word
