80eb22b54008d551f05f6b9c09951605331bfd38
Ian Kronquist <iankronquist@gmail.com>
2014-08-01 12:15:24 -0700

    Completely rewrite reporting mechanism
    report.py is a script which analyzes data using spicedham
    and displays statistics about the effectiveness of the library.
    It can be trained on the corpus tarball, and can analyze the corpus and
    arbitrary input api data.

---
+++ report.py
@@ -0,0 +1,127 @@
+import os
+import tarfile
+import json
+import requests
+from datetime import datetime
+
+from spicedham import SpicedHam
+
+sh = SpicedHam()
+
+def setUp(tarball='corpus.tar.gz', test_data_dir='corpus', backup_url='https://dl.dropboxusercontent.com/u/84239013/corpus.tar.gz'):
+    if os.path.exists(test_data_dir):
+        pass
+    elif os.path.exists(tarball):
+        tr = tarfile.open(tarball)
+        tr.extractall()
+        tr.close()
+    else:
+        r = requests.get(backup_url)
+        s = r.read()
+        f = open(tarball, 'wb')
+        f.write(s)
+        setup()
+        return
+
+
+def train_on(dir_name, is_spam):
+    print 'training with the belief that everything in {} is spam is {}'.format(
+        dir_name, is_spam)
+    for file_name in os.listdir(dir_name):
+        data = json.load(open(os.path.join(dir_name, file_name)))
+        sh.train(data, is_spam)
+
+
+def test_on_training_data():
+    print 'testing against the training data set'
+    test_on_data(os.path.join('corpus', 'train'))
+
+
+def test_on_control_data():
+    print 'testing against the control data set'
+    test_on_data(os.path.join('corpus', 'control'))
+
+
+def test_on_data(test_data_dir):
+    _test_all_files_in_dir(
+        os.path.join(test_data_dir, 'spam'), True)
+    _test_all_files_in_dir(
+        os.path.join(test_data_dir, 'ham'), False)
+
+
+def _test_all_files_in_dir(data_dir, should_be_spam):
+    test_results = {
+        'Total': [],
+        'True+': [],
+        'False+': [],
+        'True-': [],
+        'False-': [],
+        'Error': [],
+    }
+    tuning_factor = 0.5
+    for filename in os.listdir(data_dir):
+        f = open(os.path.join(data_dir,  filename), 'r')
+        json_response = json.load(f)
+        probability = sh.is_spam(json_response)
+        test_results['Total'].append(filename)
+        if 0.0 > probability > 1.0:
+            test_results['Error'].append(filename)
+        if should_be_spam:
+            if probability > tuning_factor:
+                test_results['True+'].append(filename)
+            else:
+                test_results['False-'].append(filename)
+        else:
+            if probability < tuning_factor:
+                test_results['True-'].append(filename)
+            else:
+                test_results['False+'].append(filename)
+    show_results(test_results)
+
+def show_results(test_results):
+    red = '\033[0;31m'
+    green = '\033[0;32m'
+    nocolor = '\033[0m'
+    print '{} responses analyzed'.format(len(test_results['Total']))
+    print 'True positives:  {} ({}%)'.format(len(test_results['True+']),
+        percent(len(test_results['True+']), len(test_results['Total'])))
+    print 'False negatives: {} ({}%)'.format(len(test_results['False-']),
+        percent(len(test_results['False-']), len(test_results['Total'])))
+    print 'True negatives:  {} ({}%)'.format(len(test_results['True-']),
+        percent(len(test_results['True-']), len(test_results['Total'])))
+    print 'False positives: {} ({}%)'.format(len(test_results['False+']),
+        percent(len(test_results['False+']), len(test_results['Total'])))
+    print 'Errors:          {} ({}%)'.format(len(test_results['Error']),
+        percent(len(test_results['Error']), len(test_results['Total'])))
+
+def percent(numerator, denominator):
+    if denominator == 0: return 0
+    return round(float(numerator) / float(denominator) * 100, 3)
+
+def test_on_api_data(url='https://input.mozilla.org/api/v1/feedback/'):
+    reqs = requests.get(url)
+    resps = reqs.json()
+    numSpam = 0
+    numTotal = resps['count']
+    for resp in resps['results']:
+        probability = sh.is_spam(resp)
+        if probability > 0.5:
+            numSpam += 1
+            resp['spam'] = True
+        else:
+            resp['spam'] = False
+    f = open('anaylzed-api-data' + str(datetime.now()) + '.json', 'w')
+    json.dump(resps, f)
+    print 'api'
+    print '{} api responses anaylzed.'.format(numTotal)
+    print 'Tagged Spam: {} ({}%)'.format(numSpam, percent(numSpam, numTotal))
+    print 'Tagged Hpam: {} ({}%)'.format(numTotal - numSpam,
+        percent(numTotal - numSpam, numTotal))
+
+if __name__ == '__main__':
+    setUp()
+    train_on(os.path.join('corpus', 'train', 'spam'), True)
+    train_on(os.path.join('corpus', 'train', 'ham'), False)
+    test_on_training_data()
+    test_on_control_data()
+    test_on_api_data()
