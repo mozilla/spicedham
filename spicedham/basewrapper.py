6905675c07c31607d881094bdde9bab1408eac26
Ian Kronquist <iankronquist@gmail.com>
2014-08-08 11:41:22 -0700

    Add selfs
    Oops.

--- spicedham/basewrapper.py
+++ spicedham/basewrapper.py
@@ -1,17 +1,17 @@
 
 class BaseWrapper(object):
 
-    def set_up():
+    def set_up(self):
         pass
 
-    def get_key(key, default=None):
+    def get_key(self, key, default=None):
         raise NotImplementedError()
 
-    def get_key_list(keys, default=None):
-        return [ get_key(key, default) for key in keys ]
+    def get_key_list(self, keys, default=None):
+        return [ self.get_key(key, default) for key in keys ]
 
-    def set_key_list(key_value_tuples):
-        return [ set_key(key, value) for key, value in key_value_tuples ] 
+    def set_key_list(self, key_value_tuples):
+        return [ self.set_key(key, value) for key, value in key_value_tuples ]
 
-    def set_key(key, value):
+    def set_key(self, key, value):
         raise NotImplementedError()
