da05390fa11a12d0491caff18d38e71a1e134b82
Ian Kronquist <iankronquist@gmail.com>
2014-08-21 15:34:13 -0700

    Make tag and key be a composite primary key

--- spicedham/sqlalchemywrapper/models.py
+++ spicedham/sqlalchemywrapper/models.py
@@ -1,16 +1,15 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.schema import UniqueConstraint
+from sqlalchemy.schema import PrimaryKeyConstraint
 
 Base = declarative_base()
 
 class Store(Base):
     __tablename__ = 'store'
-    id = Column(Integer, primary_key=True, autoincrement=True)
     key = Column(String)
     tag = Column(String)
     value = Column(String)
-    __table_args__ = (UniqueConstraint('key', 'tag'),)
+    __table_args__ = (PrimaryKeyConstraint('key', 'tag'),)
 
     def __unicode__(self):
         return unicode(key)
