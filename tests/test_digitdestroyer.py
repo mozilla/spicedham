d64e576e74ddc68364259ed4ca941165a9038a56
Ian Kronquist <iankronquist@gmail.com>
2014-08-26 14:20:24 -0700

    Add a test for the digitdestroyer filter

---
+++ tests/test_digitdestroyer.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+from spicedham.digitdestroyer import DigitDestroyer
+
+class TestDigitDestroyer(TestCase):
+    
+    def test_classify(self):
+        dd = DigitDestroyer()
+        dd.filter_match = 1
+        dd.filter_miss = 0
+        match_message = ['1', '2', '3', '1', '1']
+        miss_message = ['a', '100']
+        self.assertEqual(dd.classify('tag', match_message), 1)
+        self.assertEqual(dd.classify('tag', miss_message), 0)
+
