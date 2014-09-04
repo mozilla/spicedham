c3fdc1bdd689bd07c944d312691283080f491aaf
Ian Kronquist <iankronquist@gmail.com>
2014-09-03 17:28:30 -0700

    Update tests to use Spicedham class

--- tests/test_digitdestroyer.py
+++ tests/test_digitdestroyer.py
@@ -1,11 +1,13 @@
 from unittest import TestCase
 
+from spicedham import Spicedham
 from spicedham.digitdestroyer import DigitDestroyer
 
 class TestDigitDestroyer(TestCase):
     
     def test_classify(self):
-        dd = DigitDestroyer()
+        sh = Spicedham()
+        dd = DigitDestroyer(sh)
         dd.filter_match = 1
         dd.filter_miss = 0
         match_message = ['1', '2', '3', '1', '1']
