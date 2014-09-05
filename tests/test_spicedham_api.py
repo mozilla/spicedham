c6d06cc473ead6361aad364205e8e7fb1412a531
Ian Kronquist <iankronquist@gmail.com>
2014-09-05 14:36:04 -0700

    Add tests for the Spicedham object's methods

--- tests/test_spicedham_api.py
+++ tests/test_spicedham_api.py
@@ -1,94 +1,137 @@
 from unittest import TestCase
 
-from spicedham import Spicedham
+from spicedham import Spicedham, NoBackendFoundError
+from spicedham.sqlalchemywrapper import SqlAlchemyWrapper
 from mock import Mock, patch
 
 
 class TestSpicedHamAPI(TestCase):
 
-    @patch('spicedham.Spicedham._classifier_plugins')
-    def test_classify(self, mock_plugins):
-        sh = Spicedham()
+    @patch('spicedham.Spicedham.all_subclasses')
+    def test_classify(self, mock_all_subclasses):
         plugin0 = Mock()
-        plugin0.classify.return_value = .5
+        plugin0obj = Mock()
+        plugin0.return_value = plugin0obj
+        plugin0.__name__ = "SqlAlchemyWrapper"
+        plugin0obj.classify.return_value = .5
         plugin1 = Mock()
-        plugin1.classify.return_value = .75   
+        plugin1obj = Mock()
+        plugin1.return_value = plugin1obj
+        plugin1.__name__ = "NotSqlAlchemyWrapper"
+        plugin1obj.classify.return_value = .75
         plugin2 = Mock()
-        plugin2.classify.return_value = None
-        mock_plugins.__iter__.return_value = [plugin0, plugin1, plugin2]
+        plugin2obj = Mock()
+        plugin2.return_value = plugin2obj
+        plugin2.__name__ = "StillNotSqlAlchemyWrapper"
+        plugin2obj = Mock()
+        plugin2.return_value = plugin2obj
+        plugin2obj.classify.return_value = None
+        mock_all_subclasses.return_value = [plugin0, plugin1, plugin2]
+        sh = Spicedham()
         # Test when some plugins return numbers and some return None
         value = sh.classify(['classifying', 'data'])
         self.assertEqual(value, 0.625)
         # Test when all plugins return one
-        plugin0.classify.return_value = None
-        plugin1.classify.return_value = None   
+        plugin0obj.classify.return_value = None
+        plugin1obj.classify.return_value = None   
         value = sh.classify(['classifying', 'data'])
         self.assertEqual(value, 0)
 
-    @patch('spicedham.iter_entry_points')
-    @patch('spicedham.Spicedham.backend')
-    def test_load_backend(self, mock_backend, mock_iter_entry_points):
+    @patch('spicedham.Spicedham.all_subclasses')
+    def test_load_backend(self, mock_all_subclasses):
+        plugin0 = Mock()
+        plugin0obj = Mock()
+        plugin0.return_value = plugin0obj
+        plugin0.__name__ = "SqlAlchemyWrapper"
+        plugin0obj.classify.return_value = .5
+        plugin1 = Mock()
+        plugin1obj = Mock()
+        plugin1.return_value = plugin1obj
+        plugin1.__name__ = "NotSqlAlchemyWrapper"
+        plugin1obj.classify.return_value = .75
+        plugin2 = Mock()
+        plugin2obj = Mock()
+        plugin2.return_value = plugin2obj
+        plugin2.__name__ = "StillNotSqlAlchemyWrapper"
+        plugin2obj = Mock()
+        plugin2.return_value = plugin2obj
+        plugin2obj.classify.return_value = None
+        mock_all_subclasses.return_value = [plugin0, plugin1, plugin2]
         sh = Spicedham()
-        mock_backend = None
-        mock_django_orm = Mock()
-        mock_iter_entry_points = Mock()
-        # mock_plugin_class = Mock()
-        # mock_plugin_object = Mock()
-        # mock_plugin_class.return_value = mock_plugin_object
-        # mock_django_orm.load.return_value = mock_plugin_class()
-        #h= mock_django_orm.name = 'djangoorm'
-        # mock_sqlalchemy_orm = Mock()
-        #mock_sqlalchemy_orm.name = 'sqlalchemy'
-        #mock_plugin_class.return_value = mock_plugin_object
-        #mock_sqlalchemy_orm.load.return_value = mock_plugin_class()
-        #mock_iter_entry_points.__iter__.return_value = [mock_django_orm,
-        #    mock_sqlalchemy_orm]
-        # Test the first run with the django_orm plugin
-        
-        ret = sh._load_backend()
-        print 'mm', mock_iter_entry_points.mock_calls
-        self.assertEqual(ret, mock_plugin_object)
-        # Test the second run with the django_orm plugin
-        ret = sh._load_backend()
-        self.assertEqual(ret, mock_plugin_class.return_value)
-        # rest the backend for the next test
-        mock_backend = None
-        mock_iter_entry_points.return_value = [mock_django_orm,
-            mock_sqlalchemy_orm]
-        # Test the first run with the sqlalchemy plugin
-        ret = sh._load_backend()
-        self.assertEqual(ret, mock_plugin_class.return_value)
-        # Test the second run with the sqlalchemy plugin
-        ret = sh._load_backend()
-        self.assertEqual(ret, mock_plugin_class.return_value)
-        
+        # Test when some plugins return numbers and some return None
+        value = sh.train(['classifying', 'data'], True)
+        self.assertTrue(plugin0obj.train.called)
+       
 
         
     @patch('spicedham.Spicedham._load_backend')
-    @patch('spicedham.iter_entry_points')
-    def test_load_plugins(self, mock_iter_entry_points, mock_load_backend):
-         #plugin0 = Mock()
-         plugin0Object = Mock()
-         #plugin0Class = Mock(return_value=plugin0Object)
-         #plugin0.load = Mock(return_value=plugin0Class)
-         #plugin1 = Mock()
-         plugin1Object = Mock()
-         #plugin1Class = Mock(return_value=plugin1Object)
-         #plugin1.load = Mock(return_value=plugin1Class)
-         #plugin2 = Mock()
-         plugin2Object = Mock()
-         #plugin2Class = Mock(return_value=plugin2Object)
-         #plugin2.load = Mock(return_value=plugin2Class)
-         input_plugins = [plugin0Object, plugin1Object, plugin2Object]
-         expected_plugins = [plugin0Object.load, plugin1Object.load, plugin2Object.load]
-         mock_iter_entry_points.return_value = input_plugins
-         self.assertEqual(spicedham._plugins, None)
-         # now load the plugins
-         load_plugins()
-         mock_iter_entry_points.assert_called_with(group='spicedham.classifiers', name=None)
-         self.assertEqual(spicedham._plugins, expected_plugins)
-         # now load the plugins again, they should not change
-         mock_iter_entry_points.called = False
-         load_plugins()
-         self.assertEqual(mock_iter_entry_points.called, False)
-         self.assertEqual(spicedham._plugins, input_plugins)
+    def test_load_plugins(self, mock_load_backend):
+        plugin0 = Mock()
+        plugin0obj = Mock()
+        plugin0.return_value = plugin0obj
+        plugin0.__name__ = "SqlAlchemyWrapper"
+        plugin0obj.classify.return_value = .5
+        plugin1 = Mock()
+        plugin1obj = Mock()
+        plugin1.return_value = plugin1obj
+        plugin1.__name__ = "NotSqlAlchemyWrapper"
+        plugin1obj.classify.return_value = .75
+        plugin2 = Mock()
+        plugin2obj = Mock()
+        plugin2.return_value = plugin2obj
+        plugin2.__name__ = "StillNotSqlAlchemyWrapper"
+        plugin2obj = Mock()
+        plugin2.return_value = plugin2obj
+        plugin2obj.classify.return_value = None
+        mock_all_subclasses.return_value = [plugin0, plugin1, plugin2]
+        sh = Spicedham()
+        # Test when some plugins return numbers and some return None
+        value = sh.train(['classifying', 'data'], True)
+        self.assertTrue(plugin0obj.train.called)
+
+    @patch('spicedham.Spicedham.all_subclasses')
+    @patch('spicedham.Spicedham._load_backend')
+    def test_load_plugins(self, mock_all_subclasses, mock_load_backend):
+        mock_load_backend = None
+        plugin0 = Mock()
+        plugin1 = Mock()
+        plugin2 = Mock()
+        mock_all_subclasses.return_value = [plugin0, plugin1, plugin2]
+        sh = Spicedham()
+        result = sh._load_backend()
+        self.assertIn(plugin0, result)
+        self.assertIn(plugin1, result)
+        self.assertIn(plugin2, result)
+        
+
+    @patch('spicedham.Spicedham.all_subclasses')
+    def test_load_backend(self, mock_all_subclasses):
+        backend0 = Mock()
+        backend0.__name__ = 'SqlAlchemyWrapper'
+        backend0Returns = Mock()
+        backend0.return_value = backend0Returns
+        backend1 = Mock()
+        backend1.__name__ = 'NotSqlAlchemyWrapper'
+        backend2 = Mock()
+        backend2.__name__ = 'StillNotSqlAlchemyWrapper'
+        mock_all_subclasses.return_value = [backend0, backend1, backend2]
+        sh = Spicedham()
+        sh._load_backend()
+        self.assertEqual(sh.backend, backend0Returns)
+        sh = Spicedham()
+        mock_all_subclasses.return_value = []
+        self.assertRaises(NoBackendFoundError, sh._load_backend)
+
+
+    def test_all_subclasses(self):
+        class parent(object):
+            pass
+        class child0(parent):
+            pass
+        class child1(parent):
+            pass
+        sh = Spicedham()
+        result = sh.all_subclasses(parent)
+        self.assertIn(child0, result)
+        self.assertIn(child1, result)
+        self.assertEqual(2, len(result))
