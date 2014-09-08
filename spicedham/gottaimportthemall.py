f8b270e5ad8e2bb94da4c5d2620b8ba4fd541b17
Ian Kronquist <iankronquist@gmail.com>
2014-09-08 14:10:41 -0700

    Import the split_tokenizer

--- spicedham/gottaimportthemall.py
+++ spicedham/gottaimportthemall.py
@@ -4,5 +4,7 @@ import spicedham.nonsensefilter
 
 try:
     from spicedham.sqlalchemywrapper import SqlAlchemyWrapper
+    # As part of the default configuration
+    import spicedham.split_tokenizer
 except ImportError:
     pass
