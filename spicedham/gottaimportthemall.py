871e235ae8d8504b2164d5ebce2f4cc24a3d9fa8
Ian Kronquist <iankronquist@gmail.com>
2014-09-09 10:37:12 -0700

    Import error should not block tockenizer

--- spicedham/gottaimportthemall.py
+++ spicedham/gottaimportthemall.py
@@ -1,10 +1,9 @@
 import spicedham.bayes
 import spicedham.digitdestroyer
 import spicedham.nonsensefilter
+import spicedham.split_tokenizer
 
 try:
     from spicedham.sqlalchemywrapper import SqlAlchemyWrapper
-    # As part of the default configuration
-    import spicedham.split_tokenizer
 except ImportError:
     pass
