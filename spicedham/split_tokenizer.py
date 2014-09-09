d82ccc9eed66510cd5166acf91073bad50acbeaf
Ian Kronquist <iankronquist@gmail.com>
2014-09-09 10:40:16 -0700

    Hoist the docstring from the method into the class

--- spicedham/split_tokenizer.py
+++ spicedham/split_tokenizer.py
@@ -3,12 +3,12 @@ from re import split
 from spicedham.tokenizer import BaseTokenizer
 
 class SplitTokenizer(BaseTokenizer):
-    
+    """
+    Split the text on punctuation and newlines, lowercase everything, and
+    filter the empty strings
+    """
+   
     def tokenize(self, text):
-        """
-        Split the text on punctuation and newlines, lowercase everything, and
-        filter the empty strings
-        """
         text = split('[ ,.?!\n\r]', text)
         is_not_blank = lambda x: x != ''
         text = filter(is_not_blank, text)
