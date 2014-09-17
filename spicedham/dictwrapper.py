8d01d9c2163710a78df9b265309a171393293e5f
Ian Kronquist <iankronquist@gmail.com>
2014-09-16 20:19:42 -0700

    Be smarter about keys

--- spicedham/dictwrapper.py
+++ spicedham/dictwrapper.py
@@ -1,5 +1,6 @@
 from spicedham.backend import BaseBackend
 
+
 class DictWrapper(BaseBackend):
     """
     A stupid simple in memory datastore using a dictionary.
@@ -8,10 +9,10 @@ class DictWrapper(BaseBackend):
     store = {}
 
     def set_key(self, classifier, key, value):
-        self.store[classifier + key] = value
+        self.store[(classifier, key)] = value
 
     def get_key(self, classifier, key, default=None):
-        return self.store.get(classifier + key, default)
+        return self.store.get((classifier, key), default)
 
     def reset(self):
         self.store = {}
