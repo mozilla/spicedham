7deaaf80becb2d898d7672eca09f637c6711c015
Ian Kronquist <iankronquist@gmail.com>
2014-09-17 15:00:25 -0700

    Flake8 cleanup

--- spicedham/sqlalchemywrapper/models.py
+++ spicedham/sqlalchemywrapper/models.py
@@ -1,10 +1,12 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.schema import PrimaryKeyConstraint
+
 
 # TODO: Will doesn't want things to run on module import, but I believe this
 # is what the sqlalchemy docs say to do
 Base = declarative_base()
+
 
 class Store(Base):
     """
@@ -18,6 +20,6 @@ class Store(Base):
     value = Column(String)
     __table_args__ = (PrimaryKeyConstraint('key', 'classifier'),)
 
-    #TODO: Add the tag here
+    # TODO: Add the tag here
     def __unicode__(self):
-        return unicode(key)
+        return unicode(self.key)
