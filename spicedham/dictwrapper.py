6b8beb0dd8f8791b53fa22d5243f4e3ffb1b8989
Ian Kronquist <iankronquist@gmail.com>
2014-09-16 20:13:15 -0700

    Docstring fixes

--- spicedham/dictwrapper.py
+++ spicedham/dictwrapper.py
@@ -1,23 +1,17 @@
 from spicedham.backend import BaseBackend
 
 class DictWrapper(BaseBackend):
+    """
+    A stupid simple in memory datastore using a dictionary.
+    """
 
     store = {}
 
     def set_key(self, classifier, key, value):
-        """
-        Classifier and key are strings. value may be anything.
-        """
         self.store[classifier + key] = value
 
     def get_key(self, classifier, key, default=None):
-        """
-        Classifier and key are strings. returned value may be anything.
-        """
         return self.store.get(classifier + key, default)
 
     def reset(self):
-        """
-        Removes everything from the backend.
-        """
         self.store = {}
