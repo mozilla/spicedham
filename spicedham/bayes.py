6b3058c56275c7b1c47c96ff8e40f8d9d6433c58
Ian Kronquist <iankronquist@gmail.com>
2014-09-16 17:54:36 -0700

    Logging simplification and flake8  pass

--- spicedham/bayes.py
+++ spicedham/bayes.py
@@ -2,10 +2,7 @@
 # python 3 style division avoids the need to cast to floats
 # absolutely everywhere
 from __future__ import division
-import re
-import json
 import logging
-
 
 
 from spicedham.plugin import BasePlugin
@@ -19,6 +16,9 @@ class NotYetTrainedError(Exception):
     pass
 
 
+log = logging.getLogger('spicedham')
+
+
 class Bayes(BasePlugin):
     """
     A Bayesian classifier plugin
@@ -26,9 +26,6 @@ class Bayes(BasePlugin):
 
     def __init__(self, config, backend):
         self.backend = backend
-        self.log = logging.getLogger('spicedham')
-        logging.basicConfig(filename='sh_bayes_classifier.log',
-                            level=logging.DEBUG)
 
     def train(self, result, is_spam):
         """
@@ -36,11 +33,11 @@ class Bayes(BasePlugin):
         """
         # * is a special key representing all results. This is kind of a hack.
         total = self.backend.get_key(self.__class__.__name__,
-            '*', {'numSpam': 0, 'numTotal': 0})
+                                     '*', {'numSpam': 0, 'numTotal': 0})
         results = []
         for item in set(result):
             value = self.backend.get_key(self.__class__.__name__, item,
-                {'numSpam': 0, 'numTotal': 0})
+                                         {'numSpam': 0, 'numTotal': 0})
             total['numTotal'] += 1
             value['numTotal'] += 1
             if is_spam:
@@ -63,24 +60,28 @@ class Bayes(BasePlugin):
         pHam = 1.0 - pSpam
         pSpamGivenWord = pSpam
         pHamGivenWord = pHam
-        pWordList = []
         for description in set(response):
             # ignore reserved '*' or useless ''
             if description == '*' or description == '':
                 continue
             word = self.backend.get_key(self.__class__.__name__, description,
-                {'numTotal': 0, 'numSpam': 0})
+                                        {'numTotal': 0, 'numSpam': 0})
             # If there's no data on the word, ignore it
-            if word['numTotal'] == 0 or  word['numSpam'] == 0:
+            if word['numTotal'] == 0 or word['numSpam'] == 0:
                 continue
             # TODO: make an exception, not just an assert
             assert word['numTotal'] >= word['numSpam']
             pWord = (word['numTotal'] / total['numTotal'])
             pWordGivenSpam = (word['numSpam']) / total['numSpam']
-            pWordGivenHam = (word['numTotal'] - word['numSpam']) / (total['numTotal'] - total['numSpam'])
+            pWordGivenHam = ((word['numTotal'] - word['numSpam']) /
+                             (total['numTotal'] - total['numSpam']))
             pSpamGivenWord *= (pWordGivenSpam) / pWord
             pHamGivenWord *= pWordGivenHam / pWord
-            self.log.debug(u'word: {0} prob word: {1} prob spam: {2} prob spam: {3}'.format(unicode(description), unicode(pWord), unicode(pWordGivenSpam), unicode(pWordGivenHam), unicode(pWordGivenSpam/pWord), unicode(pWordGivenHam/pWord)))
+            log.debug(u'word: {0} prob word: {1} prob spam: {2} prob spam: \
+                {3}'.format(unicode(description), unicode(pWord),
+                            unicode(pWordGivenSpam), unicode(pWordGivenHam),
+                            unicode(pWordGivenSpam/pWord),
+                            unicode(pWordGivenHam/pWord)))
 
         p = ((pSpamGivenWord) / (pSpamGivenWord + pHamGivenWord))
         return (p)
