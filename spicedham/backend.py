38216f9d1b875c31b97c80bb9217557e67c92ff3
Ian Kronquist <iankronquist@gmail.com>
2014-09-17 19:33:13 -0700

    Add classifier type to the base class

--- spicedham/backend.py
+++ spicedham/backend.py
@@ -12,33 +12,35 @@ class BaseBackend(object):
         """
         raise NotImplementedError()
 
-    def get_key(self, classifier, key, default=None):
+
+    def get_key(self, classification_type, classifier, key, default=None):
         """
         Gets the value held by the classifier, key composite key.
         If it doesn't exist, return default.
         """
         raise NotImplementedError()
 
-    def get_key_list(self, classifier, keys, default=None):
+    def get_key_list(self, classification_type, classifier, keys, default=None):
         """
         Given a list of key, classifier pairs get all values.
         If key, classifier doesn't exist, return default.
         Subclasses can override this to make more efficient queries for bulk
         requests.
         """
-        return [self.get_key(classifier, key, default)
-                for key in keys]
+        return [self.get_key(classification_type, classifier, key, default)
+                for classifier, key in izip(repeat(classifier), keys)]
 
-    def set_key_list(self, classifier, key_value_pairs):
+    def set_key_list(self, classification_type, classifier, key_value_pairs):
         """
         Given a list of pairs of key, value  and a classifier set them all.
         Subclasses can override this to make more efficient queries for bulk
         requests.
         """
-        return [self.set_key(classifier, key, value)
-                for key, value in key_value_pairs]
+        return [self.set_key(classification_type, classifier, key, value)
+                for classifier, key, value
+                in izip(repeat(classifier), key_value_pairs)]
 
-    def set_key(self, classifier, key, value):
+    def set_key(self, classification_type, classifier, key, value):
         """
         Set the value held by the classifier, key composite key.
         """
